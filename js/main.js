5d2e80c76c422b8ce4d3a536638f34ca3d003f8c
Marisa Nanakul <mnanakul@uw.edu>
2016-04-12 22:29:23 -0700

    got first chart to render

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+$(function() {
+	d3.csv("data/antibiotics_data.csv", function(data) {
+   console.log(data);
+
+function unpack(rows, element) {
+	return rows.map(function(row) {
+		return row[element];
+	});
+
+}
+
 var trace1 = {
-	x: ["A. aerogenes", "B. abortus", "B. anthracis", "D. pneumoniae", "E. coli", "K. pneumoniae", "M. tuberculosis", "P. vulgaris", "P. aeruginosa", "S. typhosa", "S. schottmuelleri", "S. albus", "S. aureus", "S. fecalis", "S. hemolyticus", "S. viridans"],
-	y: [870, 1, 0.001, 0.005, 100, 850, 800, 3, 850, 1, 10, 0.007, 0.03, 1, 0.001, 0.005],
+	x: unpack(data, 'Bacteria '),
+	y: unpack(data, 'Penicilin'),
 	name: "Penicilin",
 	orientation: "v",
 	showlegend: true,
@@ -12,8 +23,8 @@ var trace1 = {
 };
 
 var trace2 = {
-	x: ["A. aerogenes", "B. abortus", "B. anthracis", "D. pneumoniae", "E. coli", "K. pneumoniae", "M. tuberculosis", "P. vulgaris", "P. aeruginosa", "S. typhosa", "S. schottmuelleri", "S. albus", "S. aureus", "S. fecalis", "S. hemolyticus", "S. viridans"],
-	y: [1, 2, 0.01, 11, 0.4, 1.2, 5, 0.1, 2, 0.4, 0.8, 0.1, 0.03, 1, 14, 10],
+	x: unpack(data, 'Bacteria '),
+	y: unpack(data, 'Streptomycin '),
 	name: "Streptomycin",
 	orientation: "v",
 	showlegend: true,
@@ -25,8 +36,8 @@ var trace2 = {
 };
 
 var trace3 = {
-	x: ["A. aerogenes", "B. abortus", "B. anthracis", "D. pneumoniae", "E. coli", "K. pneumoniae", "M. tuberculosis", "P. vulgaris", "P. aeruginosa", "S. typhosa", "S. schottmuelleri", "S. albus", "S. aureus", "S. fecalis", "S. hemolyticus", "S. viridans"],
-	y: [1.6, 0.02, 0.007, 10, 0.1, 1, 2, 0.1, 0.4, 0.008, 0.09, 0.001, 0.001, 0.1, 10, 40],
+	x: unpack(data, 'Bacteria '),
+	y: unpack(data, 'Neomycin'),
 	name: "Neomycin",
 	orientation: "v",
 	showlegend: true,
@@ -36,9 +47,7 @@ var trace3 = {
 	//yaxis: "y1"
 };
 
-var data = [trace1, trace2, trace3];
-
-
+var build = [trace1, trace2, trace3];
 
 var layout = {
 	barmode: "stack",
@@ -54,7 +63,33 @@ var layout = {
 
 };
 
-Plotly.plot('chart1', data, layout, {staticPlot: true});
+Plotly.plot('chart1', build, layout, {staticPlot: true});
+
+
+
+
+var data2 = [
+	{
+		labels: ["negative", "positive"],
+		type: "pie",
+		values: ["1", "1", "2", "2", "1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2"]
+
+
+
+
+	}
+
+
+];
+var layout2 = {
+	title: "Pie Chart"
+};
+Plotly.plot('chart2', data2, layout2);
+
+});
+});
+
+
 
 
 
